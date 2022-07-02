80689b5e1c4157460e7a311be3aa569ac04d90e0
Kiyoka Nishiyama <kiyoka@sumibi.org>
2022-07-02 07:35:29 +0000

    Added transform function.
    Implementation of transform function.

--- src/muya/lib/parser/marked/index.js
+++ src/muya/lib/parser/marked/index.js
@@ -1,11 +1,30 @@
 import { marked as upstreamMarked, Renderer, Lexer, Parser, Slugger } from 'marked'
 
-/**
- * Marked
- */
-
 function marked (src, opt = {}) {
   return upstreamMarked.parse(src, opt)
+}
+
+/*
+** Muya custom functions
+*/
+export const muyaTransformTokens = (tokens) => {
+  let token
+  let retTokens = []
+  while ((token = tokens.shift())) {
+    let newToken
+    switch (token.type) {
+      case 'heading': {
+        token.headingStyle = 'atx'
+        break
+      }
+      default:
+        break
+    }
+    delete token.tokens
+    delete token.raw
+    retTokens.push(token)
+  }
+  return retTokens
 }
 
 export {
--- test/backwardCompatibility/run.compatibility.js
+++ test/backwardCompatibility/run.compatibility.js
@@ -1,13 +1,12 @@
 import fs from 'fs'
 import path from 'path'
 import { Lexer as oldLexer } from '../../oldsrc/muya/lib/parser/marked'
-import { Lexer as newLexer } from '../../src/muya/lib/parser/marked'
+import { Lexer as newLexer, muyaTransformTokens } from '../../src/muya/lib/parser/marked'
 
 const loadMarkdownContent = pathname => {
   // Load file and ensure LF line endings.
   return fs.readFileSync(path.resolve('test/unit/data', pathname), 'utf-8').replace(/(?:\r\n|\n)/g, '\n')
 }
-
 
 // save tokentree as json file.
 const saveTokentrees = function(name,markdownStr) {
@@ -18,8 +17,9 @@ const saveTokentrees = function(name,markdownStr) {
     superSubScript: false
   }
 
-  const tokensByOld = new oldLexer(options).lex(markdownStr)
-  const tokensByNew = new newLexer(options).lex(markdownStr)
+  const tokensByOld     = new oldLexer(options).lex(markdownStr)
+  const tokensByNewTemp = new newLexer(options).lex(markdownStr)
+  const tokensByNew = muyaTransformTokens(tokensByNewTemp)
 
   const basePath = 'test/backwardCompatibility/tokentree'
   const strOld = JSON.stringify(tokensByOld,null,' ')
@@ -28,14 +28,15 @@ const saveTokentrees = function(name,markdownStr) {
   fs.writeFileSync(path.resolve(basePath,name + ".new.json"),strNew)
 }
 
-// loading
-export const BasicTextFormattingTemplate = () => {
-  return loadMarkdownContent('common/BasicTextFormatting.md')
-}
-export const HeadingsTemplate = () => {
-  return loadMarkdownContent('common/Headings.md')
-}
-
 // save results
-saveTokentrees('basictextformatting',BasicTextFormattingTemplate())
-saveTokentrees('headings',HeadingsTemplate())
+saveTokentrees('common.BasicTextFormatting',    loadMarkdownContent('common/BasicTextFormatting.md'))
+saveTokentrees('common.Blockquotes',            loadMarkdownContent('common/Blockquotes.md'))
+saveTokentrees('common.CodeBlocks',             loadMarkdownContent('common/CodeBlocks.md'))
+saveTokentrees('common.Escapes',                loadMarkdownContent('common/Escapes.md'))
+saveTokentrees('common.Headings',               loadMarkdownContent('common/Headings.md'))
+saveTokentrees('common.Images',                 loadMarkdownContent('common/Images.md'))
+saveTokentrees('common.Links',                  loadMarkdownContent('common/Links.md'))
+saveTokentrees('common.Lists',                  loadMarkdownContent('common/Lists.md'))
+saveTokentrees('gfm.BasicTextFormatting',       loadMarkdownContent('gfm/BasicTextFormatting.md'))
+saveTokentrees('gfm.Lists',                     loadMarkdownContent('gfm/Lists.md'))
+saveTokentrees('gfm.Tables',                    loadMarkdownContent('gfm/Tables.md'))
